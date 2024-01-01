e1b6700cf6b8f00b496db10bf133e41282aa2215
Asfak Ahmed <asfakahmed680@gmail.com>
2024-01-01 09:43:12 +0600

    partner section done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/Components/Navbar";
 import Hero from "@/Components/Hero";
+import Partner from "@/Components/Partner";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Partner />
     </div>
   );
 };
---
+++ src/Components/Partner.tsx
@@ -0,0 +1,11 @@
+const Partner = () => {
+  return (
+    <section className="w-full max-w-[1400px] mx-auto bg-primary-100 px-6 flex items-center gap-24 py-8">
+      <img src="SponsorRedBull.png" alt="sopnsor/image" />
+      <img src="SponsorForbes.png" alt="sopnsor/image" />
+      <img src="SponsorFortune.png" alt="sopnsor/image" />
+    </section>
+  );
+};
+
+export default Partner;
