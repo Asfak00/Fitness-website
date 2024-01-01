5d2f66a9416d76ee1c3623413fe61ed068291e54
Asfak Ahmed <asfakahmed680@gmail.com>
2024-01-01 09:03:47 +0600

    navbar done

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,23 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className=" bg-gray-20 py-6 px-6 max-w-[1400px] mx-auto flex items-center justify-between w-full">
+      <img src="/Logo.png" alt="logo" className="w-[140px]" />
+
+      <ul className="flex items-center gap-8 capitalize text-[1.1rem] font-[400]">
+        <li>home</li>
+        <li>benefits</li>
+        <li>our classes</li>
+        <li>contact us</li>
+      </ul>
+
+      <div className="flex items-center gap-5">
+        <button className="text-[1.1rem] capitalize">sign in</button>
+        <button className="py-2 px-6 bg-secondary-500 capitalize text-gray-500 rounded ">
+          become a member
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
