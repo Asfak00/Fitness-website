ab547e35b7e7a202bd2f373572a30ccbb68a8953
Asfak Ahmed <asfakahmed680@gmail.com>
2024-01-01 09:58:17 +0600

    partner section full done

--- src/Components/Partner.tsx
+++ src/Components/Partner.tsx
@@ -1,9 +1,20 @@
 const Partner = () => {
   return (
-    <section className="w-full max-w-[1400px] mx-auto bg-primary-100 px-6 flex items-center gap-24 py-8">
-      <img src="SponsorRedBull.png" alt="sopnsor/image" />
-      <img src="SponsorForbes.png" alt="sopnsor/image" />
-      <img src="SponsorFortune.png" alt="sopnsor/image" />
+    <section className="w-full max-w-[1400px] mx-auto bg-primary-100 px-6 flex items-center justify-between py-2">
+      <img src="SponsorRedBull.png" alt="sopnsor/image" className="w-[70px]" />
+      <img
+        src="Sponsorsportart.png"
+        alt="sopnsor/image"
+        className="w-[170px]"
+      />
+      <img
+        src="Sponsorlifefitness.png"
+        alt="sopnsor/image"
+        className="w-[170px]"
+      />
+      <img src="Sponsorkinomap.png" alt="sopnsor/image" className="w-[170px]" />
+      <img src="Sponsorhorizon.png" alt="sopnsor/image" className="w-[170px]" />
+      <img src="Sponsortrue.png" alt="sopnsor/image" className="w-[170px]" />
     </section>
   );
 };
