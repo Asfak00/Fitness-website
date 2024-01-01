b4a88e755bc387f61ba6c02bae86181d8d72e1c9
Asfak Ahmed <asfakahmed680@gmail.com>
2024-01-01 09:24:33 +0600

    hero section done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/Components/Navbar";
+import Hero from "@/Components/Hero";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
---
+++ src/Components/Hero.tsx
@@ -0,0 +1,33 @@
+const Hero = () => {
+  return (
+    <main className="bg-white flex items-center justify-between w-full max-w-[1400px] mx-auto px-6">
+      <div className="w-[50%] flex flex-col gap-6">
+        <h1 className="font-[700] text-[6.5rem] text-gray-500 uppercase leading-[1rem]">
+          evogym
+        </h1>
+        <p className="text-[3rem] font-[400]">evolutionary fitness</p>
+        <p className="w-[80%]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quaerat
+          delectus iure voluptatum voluptates libero tenetur corrupti recusandae
+        </p>
+        <div className="flex items-center gap-4 mt-4">
+          <button className="py-2 px-6 bg-secondary-500 capitalize text-gray-500 rounded hover:bg-white hover:border-secondary-500 duration-300 transition-all border ">
+            join now
+          </button>
+          <button className="py-2 px-6 border border-secondary-500 capitalize text-gray-500 rounded hover:bg-secondary-500 transition-all duration-300 cursor-pointer ">
+            learn more
+          </button>
+        </div>
+      </div>
+      <div>
+        <img
+          src="/HomePageGraphic.png"
+          alt="hero/image"
+          className="w-[550px]"
+        />
+      </div>
+    </main>
+  );
+};
+
+export default Hero;
